9407a413480a4ed4f2dc2036b619a81d9ae5886f
Stefano Dallona <stefano.dallona@electrolux.com>
2023-03-17 18:26:33 +0100

    Added app file

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
 from flask import Flask
+from flask_cors import CORS
+
+import logging
+
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s.%(funcName)s(): %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
 app = Flask(__name__)
+CORS(app)
+logging.basicConfig(level=logging.DEBUG)
 
-@app.route('/')
-def hello_world():
-    return '<h1>Hello from Flask & Docker</h2>'
+from .ui.rest.configuration_api import configuration_api
+from .ui.rest.execution_api import execution_api
+from .ui.rest.analysis_api import analysis_api
 
+app.register_blueprint(configuration_api)
+app.register_blueprint(execution_api)
+app.register_blueprint(analysis_api)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+CORS(configuration_api)
+CORS(execution_api)
+CORS(analysis_api)
 
+if __name__ == '__main__':
+    app.run()
